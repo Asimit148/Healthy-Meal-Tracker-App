ded3279c7e3010481dbc5636ff5ef9e2d5ebe377
Asimit148 <shrivastavaasimit@gmail.com>
2025-06-17 12:05:51 +0200

    Update welcome screen in Kt file and Basic Build for activity meal input screen in xml file.

--- app/src/main/java/com/example/healthymealtrackerapp/MainActivity.kt
+++ app/src/main/java/com/example/healthymealtrackerapp/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.healthymealtrackerapp.MealInput
 import com.example.healthymealtrackerapp.R
 
 class MainActivity : AppCompatActivity() {
@@ -29,5 +30,4 @@ class MainActivity : AppCompatActivity() {
             finishAffinity()
         }
     }
-}
 }
