ab9dfdb793327f3a62f225755beefdee6e1b9848
Asimit148 <shrivastavaasimit@gmail.com>
2025-06-17 11:47:45 +0200

    Basic Build for welcome screen in Kt file.

--- app/src/main/java/com/example/healthymealtrackerapp/MainActivity.kt
+++ app/src/main/java/com/example/healthymealtrackerapp/MainActivity.kt
@@ -1,20 +1,33 @@
-package com.example.healthymealtrackerapp
+package com.example.weatherapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.healthymealtrackerapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Get reference started button by it's id from the xml layout!
+        val buttonNext = findViewById<Button>(R.id.buttonNext)
+        val buttonExit = findViewById<Button>(R.id.buttonExit)
+
+        buttonNext.setOnClickListener {
+            // create an intent to navigate to the PackageListActivity when the button is clicked.
+            startActivity(Intent(this, MealInput::class.java))
+        }
+
+        buttonExit.setOnClickListener {
+            // This will allow you to exit the app when clicked.
+            finishAffinity()
         }
     }
+}
 }
